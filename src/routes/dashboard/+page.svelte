afe27fbf07d0395d7f1d702526eb6fd4ae4d7cdc
Raynaldi <raynss455x@gmail.com>
2024-09-23 11:38:50 +0700

    update card

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	export let data: { total: number, firstImageUrl: string };
 	
+	export let data: { total: number; firstImageUrl: string };
+	let isHovered = false; 
+  
 	function PortoflioPath() {
 	  goto('/dashboard/portofolio');
 	}
@@ -9,20 +11,20 @@
 	function TestimonialPath() {
 	  goto('/dashboard');
 	}
-</script>
-
-
-
-<body class="bg-white">
+  </script>
+  
+  <body class="bg-white">
 	<div class="max-h-screen w-screen md:max-w-[1000px] md:max-h-[500px] mx-auto">
 	  <div class="content md:mt-10 sm:flex">
-		<button on:click={PortoflioPath} class="flex flex-row w-full h-[200px] mb-5 md:h-[300px] p-4 sm:flex-col sm:w-1/2">
-		  <div class="relative w-full bg-white shadow-lg rounded-lg overflow-hidden hover:bg-gray-200 transition duration-300">
-			<div class="absolute inset-0 flex items-center justify-center text-9xl font-bold text-gray-300 opacity-0 hover:opacity-100 transition duration-300">
-				<img src={data.firstImageUrl} alt="gambar pertama" class="object-cover h-full w-full mx-auto" />
-			</div>
+		<button
+		  on:click={PortoflioPath}
+		  class="flex flex-row w-full h-[200px] mb-5 md:h-[300px] p-4 sm:flex-col sm:w-1/2"
+		  on:mouseenter={() => isHovered = true}
+		  on:mouseleave={() => isHovered = false}
+		>
+		  <div class="relative w-full bg-white shadow-[5px_5px_0px_black] rounded-lg border-2 border-black">
 			<div class="flex flex-row h-[200px] md:h-[260px]">
-			  <div class="w-[25%] flex flex-col justify-between py-6 md:px-2 md:pt-8 md:pb-10">
+			  <div class={`w-[25%] flex flex-col justify-between py-6 md:px-2 md:pt-8 md:pb-10 transition-all duration-300 ease-in-out ${isHovered ? 'w-[0%] opacity-0' : 'opacity-100'}`}>
 				<div>
 				  <h2 class="text-md font-bold text-center">Portofolio</h2>
 				</div>
@@ -31,7 +33,7 @@
 				  <p class="text-lg font-bold text-center">{data.total}</p>
 				</div>
 			  </div>
-			  <div class="w-[75%] py-2 pr-2">
+			  <div class={`w-[75%] py-2 pr-2 transition-all duration-300 ease-in-out ${isHovered ? 'w-[100%]' : ''}`}>
 				<img src={data.firstImageUrl} alt="gambar pertama" class="object-cover h-full w-full mx-auto" />
 			  </div>
 			</div>
@@ -39,7 +41,7 @@
 		</button>
   
 		<button on:click={TestimonialPath} class="flex flex-row w-full h-[200px] md:h-[300px] p-4 sm:flex-col sm:w-1/2">
-		  <div class="relative w-full bg-white shadow-lg rounded-lg overflow-hidden hover:bg-gray-200 transition duration-300">
+		  <div class="relative w-full bg-white shadow-[5px_5px_0px_black] rounded-lg">
 			<div class="absolute inset-0 flex items-center justify-center text-9xl font-bold text-gray-300 opacity-0 hover:opacity-100 transition duration-300">
 			  2
 			</div>
@@ -61,4 +63,5 @@
 		</button>
 	  </div>
 	</div>
-</body>
+  </body>
+  
