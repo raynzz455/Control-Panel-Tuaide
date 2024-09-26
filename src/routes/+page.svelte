1fde588757a9ff6d6a971291ac7509e60bb7e0d1
Raynaldi <raynss455x@gmail.com>
2024-09-26 10:56:14 +0700

    optimize button hello

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
     import "../app.css";
     import { goto } from '$app/navigation';
 
-    function handleRedirect() {
-        goto('/dashboard');
+    async function handleRedirect() {
+       await goto('/dashboard');
     }
 
     export { handleRedirect };
@@ -40,12 +40,35 @@
         transform: scale(0);
         animation: popIn 0.5s forwards;
         animation-delay: 1.5s; 
+        padding: 16px 24px; /* Padding untuk tombol */
+        font-size: 1.2rem; /* Ukuran font default */
+        border-radius: 10px; /* Border radius yang lebih halus */
+        cursor: pointer; /* Pointer saat hover */
+        transition: background-color 0.3s ease, transform 0.2s ease; /* Transisi untuk efek hover */
+        margin-top: 12px; /* Margin atas untuk tombol */
     }
 
     @keyframes popIn {
         to {
             opacity: 1;
             transform: scale(1);
+        }
+    }
+
+    .button:hover {
+        background-color: #f47c19; /* Warna latar saat hover */
+        transform: scale(1.05); /* Sedikit membesar saat hover */
+    }
+
+    .button:active {
+        transform: scale(0.95); /* Sedikit mengecil saat diklik */
+    }
+
+    @media (min-width: 640px) { /* Untuk ukuran sm */
+        .button {
+            font-size: 1.2rem; /* Ukuran font tetap sama */
+            margin-top: 20px;
+            padding: 8px 10px; /* Margin atas yang diinginkan */
         }
     }
 </style>
@@ -55,8 +78,8 @@
         <div class="mx-auto justify-center w-[320px] h-[200px] mb-[10px]">
             <img src="/images/logo.png" alt="logo-gambar-ceuanh" class="logo w-full h-full object-cover">
         </div>
-        <button on:click={handleRedirect} class="button w-[110px] p-3 font-bold text-md text-white bg-[#f48927] rounded-xl">
+        <button on:click={handleRedirect} class="button text-white bg-[#f48927]">
             Let's Go !
         </button>
     </div>
-</body> 
+</body>
